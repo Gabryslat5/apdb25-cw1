8c78d40ad5f5433e4fe8859be6884723f2058400
Damian Prus <s31592@pjwstk.edu.pl>
2025-03-04 16:45:54 +0100

    Average

--- ConsoleApp122/ConsoleApp122/Program.cs
+++ ConsoleApp122/ConsoleApp122/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+static double GetAverage(int[] arr)
+{
+    return 0;
+}
+
+int[] arr = { 1,2,3,4,5};
+GetAverage(arr);
